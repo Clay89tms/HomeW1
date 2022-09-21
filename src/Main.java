149015b89d98bde13b0bd087ff7272768796fc8b
Clay89tms <bboyClay89@gmail.com>
2022-09-21 23:09:15 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         String a = "abc";
         String b = "aaa";
         System.out.println("Hello world!!!");
+        for (int i = 0; i<3; i++){
+            if (i == 0) System.out.print("hello");
+            if (i == 1) System.out.print(" my");
+            if (i == 2) System.out.print(" friend!");
+            if (i == 3) System.out.print(" ne vuvedet ns ekran");
+        }
     }
 }
