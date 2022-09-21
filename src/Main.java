bbeea4d55dcaeff3488f9433c901a7b50bde9648
Clay89tms <bboyClay89@gmail.com>
2022-09-21 22:59:13 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello !!!");
+        String a = "abc";
+        String b = "aaa";
+        System.out.println("Hello world!!!");
     }
 }
